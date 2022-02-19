2cfb4dfcde626f03617c74f60c580a1ee8218e14
Enrique <enrique.diaz@dox.cl>
2022-02-18 23:55:15 -0500

    initial project setup

---
+++ src/main/java/com/prodigio/vehiclemiles/VehiclemilesApplication.java
@@ -0,0 +1,13 @@
+package com.prodigio.vehiclemiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclemilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehiclemilesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prodigio/vehiclemiles/VehiclemilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prodigio.vehiclemiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclemilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
